85df4d11643ab1ff0d0dc1a1a038879bab38c70c
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-18 14:53:24 +0100

    Home screen Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+
 function App() {
   return (
     <>
       <Header />
       <main className="mx-3">
         <Container>
-          <h1>Welcome to proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
