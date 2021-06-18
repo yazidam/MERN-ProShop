6ff9b303da4db6ec9863d94e975109855a977083
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-18 12:39:01 +0100

    Header, Footer & React Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 function App() {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main className="mx-3">
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
