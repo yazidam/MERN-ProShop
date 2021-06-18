56d8af685422c50fa357702fe7633f9f23221524
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-18 23:46:21 +0100

    Connect to databas On Line

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+// export default connectDB;
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const productus = require('./data/products');
 const app = express();
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
 
 app.get('/api/products', (req, res) => {
   res.json(productus);
@@ -10,4 +16,6 @@ app.get('/api/products/:id', (req, res) => {
   const product = productus.find((p) => p._id == req.params.id);
   res.json(product);
 });
-app.listen(5000, console.log('serveur runnig in port 5000'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`serveur runnig in port ${PORT}`));
