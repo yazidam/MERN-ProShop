f3ef0a9f314577ced19add09d5f1bab50fd8a8b1
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-19 11:36:13 +0100

    Adding Models

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userShema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//  const User = mongoose.model('User', userShema)
+module.exports = mongoose.model('User', userShema);
