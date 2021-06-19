8972236e0683c82f11b6b92ea4eeebba5cd1b2d5
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-19 16:25:12 +0100

    Product list reducer, action & HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/producutActions';
 import { Row, Col } from 'react-bootstrap';
 // import products from '../products';
-import axios from 'axios';
+// import axios from 'axios';
 import Product from '../components/Product';
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
-    const fetechProduct = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetechProduct();
-  }, []);
+    dispatch(listProducts());
+    // const fetechProduct = async () => {
+    //   const { data } = await axios.get('/api/products');
+    //   setProducts(data);
+    // };
+    // fetechProduct();
+  }, [dispatch]);
+
   return (
     <>
       <h1>Lates products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-const reducer = combineReducers({}); // empty objet we don't have any reucer yet   //b3ed nzidhom
+import { productListReducer } from './reducers/productReducers';
+const reducer = combineReducers({
+  // empty objet we don't have any reducer yet   //b3ed nzidhom
+  productList: productListReducer,
+});
 
 const initialState = {}; //ay haja n7boha load when redux store load n7otouha ghadii
 
