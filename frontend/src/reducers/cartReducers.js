af466a80d74d4cfce6ed9813c7027cc8a5057121
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-19 23:05:26 +0100

    Start With Shopping Cart

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,7 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: {} }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+  }
+};
