9bbfb59ee6d82e350180717500218d5d10f67dd9
yazidam <ahmedyazid.mejri@esprit.tn>
2021-06-21 21:55:34 +0100

    User login reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,18 +6,25 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 const reducer = combineReducers({
   // empty objet we don't have any reducer yet   //b3ed nzidhom
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems')) //parsina eli mawjoud f local storge
   : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo')) //parsina eli mawjoud f local storge
+  : null;
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }; //ay haja n7boha load when redux store load n7otouha ghadii
 
 const middelware = [thunk];
