6e6de59b81b17e691fd43d63703dd2312c41b5e4
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-07-29 21:41:52 +0100

    Adding Order Controller Route

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const app = express();
-const dotenv = require('dotenv');
-const productRoute = require('./routes/productRoute');
-const userRoute = require('./routes/userRoute');
-const connectDB = require('./config/db');
+const dotenv = require("dotenv");
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRoute");
+const orderRoute = require("./routes/orderRoute");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use('/api/products', productRoute);
-app.use('/api/users', userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
+app.use("/api/orders", orderRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`serveur runnig in port ${PORT}`));
