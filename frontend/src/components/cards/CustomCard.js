81f5cd87eb8cbcffb4880d5027980dade6d2f549
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-09-01 15:52:18 +0100

    Adding hover effect to apexcharts

--- frontend/src/components/cards/CustomCard.js
+++ frontend/src/components/cards/CustomCard.js
@@ -10,4 +10,12 @@ export const CustomCard = styled(Card)`
   box-shadow: -1px 13px 17px rgba(0, 0, 0, 0.06);
   border-radius: 50px;
   border: transparent;
+
+  &:hover {
+    background-color: #f8f9fb;
+    -webkit-box-shadow: 3px 5px 27px -5px rgba(0, 0, 0, 0.59);
+    box-shadow: 3px 5px 27px -5px rgba(0, 0, 0, 0.59);
+    transition: box-shadow 0.5s ease-in-out;
+    background-color: #f8f9fb;
+  }
 `;
